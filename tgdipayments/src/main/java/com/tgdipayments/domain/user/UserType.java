7738136b41c4625dc874aaf1edc73d4d6f255a90
Guilherme <guilherme.reisfreitas@gmail.com>
2024-01-13 18:36:24 -0300

    create DTO, UserType

---
+++ tgdipayments/src/main/java/com/tgdipayments/domain/user/UserType.java
@@ -0,0 +1,6 @@
+package com.tgdipayments.domain.user;
+
+public enum UserType {
+    CLIENTE,
+    EMPRESA
+}
---
+++ tgdipayments/src/main/java/com/tgdipayments/dtos/ExceptionDTO.java
@@ -0,0 +1,4 @@
+package com.tgdipayments.dtos;
+
+public record ExceptionDTO (String message, String StatusCode) {
+}
