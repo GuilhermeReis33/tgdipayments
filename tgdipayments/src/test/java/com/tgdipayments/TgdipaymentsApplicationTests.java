bab8f18af0aefd7b2a74b8b70093ed60fc185e52
Guilherme <guilherme.reisfreitas@gmail.com>
2024-01-13 18:35:51 -0300

    initial commit

---
+++ tgdipayments/src/test/java/com/tgdipayments/TgdipaymentsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tgdipayments;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TgdipaymentsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
